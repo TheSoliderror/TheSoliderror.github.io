cc5318bbece6ab9cd738f662b0dcfff91f106826
Soliderror <83806741+TheSoliderror@users.noreply.github.com>
2022-03-12 11:02:29 -0500

    Add files via upload

---
+++ Flask H1 Server List/Install Flask.bat
@@ -0,0 +1,27 @@
+pip install APScheduler
+
+pip install click
+
+pip install Flask
+
+pip install Flask-APScheduler
+
+pip install Flask-Caching
+
+pip install itsdangerous
+
+pip install Jinja2
+
+pip install MarkupSafe
+
+pip install python-dateutil
+
+pip install pytz
+
+pip install six
+
+pip install tzlocal
+
+pip install Werkzeug
+
+pause
---
+++ Flask H1 Server List/Serverlist/Ruuner.bat
@@ -0,0 +1,4 @@
+set FLASK_APP=__init__.py 
+py -m flask run -h localhost -p 80
+
+pause
